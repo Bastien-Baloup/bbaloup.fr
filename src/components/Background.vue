1430ca9e4d150a3793ade6fbc465343be42ac367
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-04-02 01:38:59 +0200

    Add transitions to parallax

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -43,26 +43,26 @@ onUnmounted(() => {
     <img
       src="/assets/W_1/layer_1.0.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-49]"
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-49] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * -2%), calc(var(--shiftY) * -2%), 0);"
     />
     <img
       src="/assets/W_1/layer_2.0.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-48]"
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-48] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * -1%), calc(var(--shiftY) * -1%), 0);"
     />
     <img src="/assets/W_1/layer_3.0.png" alt class="min-h-full min-w-full fixed top-[-10%] z-[-47]" />
     <img
       src="/assets/W_1/layer_4.0.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-46]"
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-46] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * 1%), calc(var(--shiftY) * 1%), 0);"
     />
     <img
       src="/assets/W_1/layer_5.1.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-45]"
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-45] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * 2%), calc(var(--shiftY) * 2%), 0);"
     />
     <div class="h-[3%] w-full fixed bottom-0 bg-purple-gray-600" />
