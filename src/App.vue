ad44c5a5feb099060febc08131220c21ba119d61
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-04-02 01:28:20 +0200

    Add parallax Background

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Background from './components/Background.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Background />
 </template>
 
-<style>
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  --shiftX: 0;
+  --shiftY: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Background.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+import { watchViewport, unwatchViewport } from 'tornis'
+
+const updateValues = ({ size, mouse, orientation }) => {
+
+  var xFromCenterRatio, yFromCenterRatio
+
+  if (mouse.changed || orientation.changed) {
+    if (mouse.changed) {
+      xFromCenterRatio = (mouse.x - size.x / 2) / (size.x / 2)
+      yFromCenterRatio = (mouse.y - size.y / 2) / (size.y / 2)
+    }
+
+    if (orientation.changed) {
+      xFromCenterRatio = orientation.gamma / 22.5
+      yFromCenterRatio = orientation.beta / 22.5
+    }
+
+    document.body.style.setProperty('--shiftX', xFromCenterRatio.toFixed(2))
+    document.body.style.setProperty('--shiftY', yFromCenterRatio.toFixed(2))
+  }
+}
+
+onMounted(() => {
+  // bind the watch function
+  watchViewport(updateValues)
+})
+
+
+onUnmounted(() => {
+  // when you want to stop updating
+  unwatchViewport(updateValues)
+})
+
+
+</script>
+
+<template>
+  <!-- Background -->
+  <div class="fixed top-0 left-0 right-0 bottom-0 overflow-hidden brightness-90">
+    <img src="/assets/W_1/layer_0.png" alt class="min-h-full min-w-full fixed top-0 -z-50" />
+    <img
+      src="/assets/W_1/layer_1.0.png"
+      alt
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-49]"
+      style="transform: translate3d(calc(var(--shiftX) * -2%), calc(var(--shiftY) * -2%), 0);"
+    />
+    <img
+      src="/assets/W_1/layer_2.0.png"
+      alt
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-48]"
+      style="transform: translate3d(calc(var(--shiftX) * -1%), calc(var(--shiftY) * -1%), 0);"
+    />
+    <img src="/assets/W_1/layer_3.0.png" alt class="min-h-full min-w-full fixed top-[-10%] z-[-47]" />
+    <img
+      src="/assets/W_1/layer_4.0.png"
+      alt
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-46]"
+      style="transform: translate3d(calc(var(--shiftX) * 1%), calc(var(--shiftY) * 1%), 0);"
+    />
+    <img
+      src="/assets/W_1/layer_5.1.png"
+      alt
+      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-45]"
+      style="transform: translate3d(calc(var(--shiftX) * 2%), calc(var(--shiftY) * 2%), 0);"
+    />
+    <div class="h-[3%] w-full fixed bottom-0 bg-purple-gray-600" />
+  </div>
+</template>
