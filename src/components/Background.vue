9769506e241478e6a31265b711750e6c28b39abe
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-04-02 14:18:28 +0200

    Improve responsiveness of background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -38,33 +38,41 @@ onUnmounted(() => {
 
 <template>
   <!-- Background -->
-  <div class="fixed top-0 left-0 right-0 bottom-0 overflow-hidden brightness-90">
-    <img src="/assets/W_1/layer_0.png" alt class="min-h-full min-w-full fixed top-0 -z-50" />
+  <div class="fixed top-0 left-0 right-0 bottom-0 overflow-hidden brightness-75">
+    <img
+      src="/assets/W_1/layer_0.png"
+      alt
+      class="min-h-full md:min-h-[115%] 2xl:min-h-[125%]] min-w-full object-cover fixed top-0 xl:top-[-5%] 2xl:top-[-10%] z-[-100]"
+    />
     <img
       src="/assets/W_1/layer_1.0.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-49] transition-all duration-500 ease-out"
+      class="min-h-full md:min-h-[115%] 2xl:min-h-[125%] w-[110%] min-w-[100vh] max-w-none fixed top-0 md:top-[-15%] 2xl:top-[-25%] left-[-5%] -z-50 transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * -2%), calc(var(--shiftY) * -2%), 0);"
     />
     <img
       src="/assets/W_1/layer_2.0.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-48] transition-all duration-500 ease-out"
+      class="min-h-full md:min-h-[115%] 2xl:min-h-[125%] w-[110%] min-w-[100vh] max-w-none fixed top-0 md:top-[-15%] 2xl:top-[-25%] left-[-5%] z-[-49] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * -1%), calc(var(--shiftY) * -1%), 0);"
     />
-    <img src="/assets/W_1/layer_3.0.png" alt class="min-h-full min-w-full fixed top-[-10%] z-[-47]" />
+    <img
+      src="/assets/W_1/layer_3.0.png"
+      alt
+      class="min-h-full md:min-h-[115%] 2xl:min-h-[125%] w-[110%] min-w-[100vh] max-w-none fixed top-0 md:top-[-15%] 2xl:top-[-25%] z-[-48]"
+    />
     <img
       src="/assets/W_1/layer_4.0.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-46] transition-all duration-500 ease-out"
+      class="min-h-full md:min-h-[115%] 2xl:min-h-[125%] w-[110%] min-w-[100vh] max-w-none fixed top-0 md:top-[-15%] 2xl:top-[-25%] left-[-5%] z-[-47] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * 1%), calc(var(--shiftY) * 1%), 0);"
     />
     <img
       src="/assets/W_1/layer_5.1.png"
       alt
-      class="min-h-full min-w-[110%] fixed top-[-10%] left-[-5%] z-[-45] transition-all duration-500 ease-out"
+      class="min-h-full md:min-h-[115%] 2xl:min-h-[125%] w-[110%] min-w-[100vh] max-w-none fixed top-0 md:top-[-15%] 2xl:top-[-25%] left-[-5%] z-[-45] transition-all duration-500 ease-out"
       style="transform: translate3d(calc(var(--shiftX) * 2%), calc(var(--shiftY) * 2%), 0);"
     />
-    <div class="h-[3%] w-full fixed bottom-0 bg-purple-gray-600" />
+    <div class="h-[3%] w-full fixed bottom-0 bg-purple-gray-600 z-[-46]" />
   </div>
 </template>
