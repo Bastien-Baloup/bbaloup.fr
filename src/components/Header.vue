eb35f80c839fe3ab982f139ba39f230ddc9eebc9
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-05-18 23:54:04 +0200

    Add icon to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,6 @@
 
 <template>
   <header class="fixed top-0 left-0 z-10 w-full box-border h-[10vh] flex backdrop-blur-[2px] backdrop-grayscale-[50%] bg-[#46464755] shadow-md md:h-[7.5vh]">
-    
+    <img src="icon.svg" alt="" class="m-2 ">
   </header >
 </template>
