546b24f0a8a6771374d9d60e9ee6156aa63c3a29
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-05-30 13:04:53 +0200

    Set the icon in a link to the top section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <script setup></script>
 
 <template>
-  <header class="fixed top-0 left-0 z-10 w-full box-border h-[10vh] flex backdrop-blur-[2px] backdrop-grayscale-[50%] bg-[#46464755] shadow-md md:h-[7.5vh]">
-    <img src="icon.svg" alt="" class="m-2 ">
-  </header >
+  <header
+    class="fixed top-0 left-0 z-10 w-full box-border h-[10vh] flex backdrop-blur-[2px] backdrop-grayscale-[50%] bg-[#46464755] shadow-md md:h-[7.5vh]">
+    <a href="#home" class="m-4 ml-5"><img src="icon.svg" alt="back to the top" class="h-full" /></a>
+  </header>
 </template>
