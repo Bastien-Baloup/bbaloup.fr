7ce5b7b46451e0e6219bbed49ab756a29b666131
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-07-09 19:36:39 +0200

    Visual changes
    Add Main title in next to the logo
    Set min header height to ensure the logo is visible on small height screens

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { NA, NButton, NIcon, NDrawer, NDrawerContent } from 'naive-ui'
+import { NA, NButton, NIcon, NDrawer, NDrawerContent, NText } from 'naive-ui'
 import { Menu } from '@vicons/ionicons5'
 
 const mobileMenuIsOpen = ref(false)
@@ -11,8 +11,11 @@ const closeMobileMenu = () => mobileMenuIsOpen.value = false
 
 <template>
   <header
-    class="fixed top-0 left-0 z-10 w-full box-border h-[10vh] flex backdrop-blur-[2px] backdrop-grayscale-[50%] bg-[#46464755] shadow-md md:h-[7.5vh]">
-    <n-a href="#home" class="flex-shrink-0 my-4 ml-5 mr-auto md:my-2 md:ml-[10vw] xl:ml-[15vw]"><img src="icon.svg" alt="back to the top" class="h-full" /></n-a>
+    class="fixed top-0 left-0 z-10 w-full box-border min-h-[4rem] h-[10vh] flex backdrop-blur-[2px] backdrop-grayscale-[50%] bg-[#46464755] shadow-md md:h-[7.5vh]">
+    <n-a href="#home" class="flex-shrink-0 my-4 ml-5 mr-auto md:my-2 md:ml-[10vw] xl:ml-[15vw] flex items-center">
+      <img src="icon.svg" alt="back to the top" class="h-full" />
+      <n-text type="primary" class="leckerli hidden xl:block text-[2rem] 2xl:text-[3rem] ml-4"><span class="lily">B</span>astien <span class="lily">B</span>aloup</n-text>
+    </n-a>
     <nav class="hidden justify-end sm:flex my-auto mr-0 text-[1.25rem] underline-offset-2 md:mr-[10vw] md:text-[1.4rem] md:underline-offset-1 xl:mr-[15vw] 2xl:underline-offset-2 2xl:text-[2rem]">
       <n-a href="#home">Accueil</n-a>
       <n-a href="#skills">Mes&nbsp;Compétences</n-a>
